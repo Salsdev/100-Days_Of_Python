efbef07a83aa0d0794b64249c49a3c68c46c224d
Salsdev <salehaliyu111@gmail.com>
2024-08-24 17:10:19 +0100

    This commit will push all files as exec

---
+++ day_2/1_data_type.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# Subscripting
+print("Hello"[0])
+
+# Sring
+print("123" + "456")
+
+# Integer = whole number
+print(123 + 345)
+
+# Large integers
+print(234_567_432)
+
+# Float = floating point number
+print(215.2)
+
+# Boolean
+print(True)
+print(False)
+
+# checking for data type
+print(type(123))
+print(type("Hello"))
+print(type(21.3))
+print(type(True))
+
+# Data type conversion
+num = 345
+new_num = float(num)
+print(new_num)
+
+# Converted int to str and concatenated it
+print("Number of letter in your name: " + str(len(input("What is your name "))))
