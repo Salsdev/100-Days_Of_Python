1a54493cc23c188403a3668c7f985238bd09105c
Salsdev <salehaliyu111@gmail.com>
2024-08-26 00:22:15 +0100

    This commit will push all files as exec

---
+++ day_3/1-control_flow.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# print("Hello, Let's Explore How CGPA Determines Your Degree Class in Nigerian Universities!")
+#user_cgpa = float(input("What is your CGPA? "))
+#if user_cgpa >= 4.50 and user_cgpa <= 4.50 :
+    #print("First class")
+#elif user_cgpa >= 3.50 and user_cgpa <= 4.49:
+    #print("Second class Upper")
+#elif user_cgpa >= 2.50 and user_cgpa <= 3.49:
+    #print("Second class upper")
+#else:
+   # print("Third class")
+# Check Odd or Even
+print("Welcome")
+number_to_check = int(input("What is the number you want to chek? "))
+checked_input = number_to_check % 2
+if check_input == 0:
+    print("Even")
+else:
+    print("Odd")
