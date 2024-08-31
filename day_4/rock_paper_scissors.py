4e3b7589a6634d40e682212db9a1cc50a1309c51
Salsdev <salehaliyu111@gmail.com>
2024-08-31 21:03:02 +0100

    This commit will push files as exec

--- day_4/rock_paper_scissors.py
+++ day_4/rock_paper_scissors.py
@@ -30,6 +30,7 @@ scissors = '''
 ---.__(___)  
 '''
 
+# Ask for user input
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
 
 game_images = [rock, paper, scissors]
@@ -40,7 +41,7 @@ print("Computer choose:")
 print(game_images[computer_choice])
 
 
-
+# Logic
 if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number. You lose!")
 elif user_choice == 0 and computer_choice == 2:
